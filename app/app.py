fix the data transfer/query but calculation in vain

app/app.py
@@ -4,38 +4,20 @@ from flask import Flask, request, render_template, abort, url_for, json, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import json
 import html          #import sqlite3
-db = SQLAlchemy()
-app = Flask(__name__, static_url_path='')   # app = flask.Flask(__name__) # coz, import style >>> import flask
-app.config["DEBUG"] = True
-from sqlalchemy.sql import text
+# db = SQLAlchemy()
+# app = Flask(__name__, static_url_path='')   # app = flask.Flask(__name__) # coz, import style >>> import flask
+# app.config["DEBUG"] = True
+# from sqlalchemy.sql import text
 """
 import the others function
 """
-# from . import db 
-# from utils import ctest
-from calculator import *  #load_manuplate # ./../
 from spcchart import *
+from spcTable import *
+from calculator import *
 import os,sys,traceback
 from sqlalchemy import create_engine 
-###########CONFIGURATION###################
-print(os.getcwd()) # print the pwd status
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:edge9527@localhost:5432/dev_tenant"
-engine = create_engine('postgresql://postgres:edge9527@localhost:5432/dev_tenant')
-connection = engine.connect()
-db.init_app(app)
-def test():
-    print("kkk")
-
-def test_u_l(u, l, v):
-    print('lol....')
-    if v > u:
-        print("no 1")
-    elif v < l:
-        print("no 2")
-    else:
-        print("ok")
-
+#------------CONFIGURATION--------------
+# print(os.getcwd()) # print the pwd status
 class PassGateway():
     # @app.route('/login', methods=['GET', 'POST'])
     # def login():
@@ -89,58 +71,8 @@ class PassGateway():
 #----------------GET-------------------
 @app.route('/query', methods=['GET'])
 def index():
-    # sql_cmd = (
-    #     '''
-    # SELECT spc_measure_point_config.name, spc_measure_point_history.value,(spc_measure_point_config.usl+spc_measure_point_config.std_value) AS USL, (spc_measure_point_config.std_value-spc_measure_point_config.lsl) AS LSL --,spc_measure_point_history.measure_object_id
-
-    # FROM spc_measure_point_config  left OUTER JOIN spc_measure_point_history
-    # ON spc_measure_point_config.uuid = spc_measure_point_history.spc_measure_point_config_uuid
-
-    # WHERE spc_measure_point_history.value NOT IN (-88888888)
-
-    # order by spc_measure_point_config.uuid;
-    #     '''  
-    # )
-    sql_cmd = (
-        """
-        SELECT value FROM spc_measure_point_history
-        WHERE value NOT IN (-88888888);
-        """
-    )
     # test()
-    # result = db.engine.execute(text("sql_cmd").execution_options(autocommit=True))
-    # print("result: ", result)
-    # # user = db.session.query().from_statement(text(sql_cmd)).params(name="").all()
-    # method_a starts a transaction and calls method_b
-    def method_a(connection):
-        with connection.begin():  # open a transaction
-            method_b(connection)
-
-    # method_b also starts a transaction
-    def method_b(connection):
-        with connection.begin(): # open a transaction - this runs in the context of method_a's transaction
-            connection.execute(sql_cmd)
-    
-
-    # # result = connection.execute(sql_cmd).first()[0]
-    resultproxy = engine.execute(sql_cmd)
-    d = [{column: value for column, value in row.items()} for row in resultproxy] # fetch all value
-    # d, a = {}, []
-    # for row in resultproxy:
-    # # row.items() returns an array like [(key0, value0), (key1, value1)]
-    #     for column, value in row.items():
-    #     # build up the dictionary
-    #         d = {**d, **{column: value}}
-    #     a.append(d)
-    # Query_value = [values for key,values in d.items()]
-    print("sql result: ", type(d))
-    
-    return d#'ok', #,result #,id_count #query_data #
-
-@app.route("/perform", methods=['GET'])
-def perform():
-  output_dict = {"success": False}
-  if request.method == "GET":
+    if request.method == "GET":
       # query params
     #   arg_n = request.args.get('n') # try to request 'n'
       # body json
@@ -163,28 +95,28 @@ def perform():
     #   print( body_json['jj'] )
     #   n =  ctest.sum1(arg_n) #nmp, trendObj = load_manpulate()
     #   print("n: ", n)
-    #   print("trendObj: ", trendObj)
-      return body_json #jsonify(index()), 200
+    #   print("trendObj: ", trendObj)   
+    return #'ok', #,result
 
-
-
-# mylst = [2 for i in range(100)]
-mylst = np.linspace(1,100,30)
-usl = 18
-lsl = 10
-results = calc(mylst, usl, lsl) # print(results)
-resultD = dict(zip(keys, results)) , index()# results turn into dict type
-# map(dict, map(lambda t:zip(('num','char'),t), zip(list_nums,list_chars)))
-
+@app.route("/perform", methods=['GET'])
+def perform():
+    # resultD = qquery()
+    usl = 10
+    lsl = 10
+#   = np.linspace(1,30,30)
+    resultA = calc(mylst = qquery(), usl=usl, lsl=lsl) ; keys = ["Cp","Cpu","Cpk","Ppk"]
+    resultD = dict(zip(keys, resultA))  #resultA turn into dict type
+  # map(dict, map(lambda t:zip(('num','char'),t), zip(list_nums,list_chars))) # [values for key,values in d.items()]
+    print("sql result: ", resultD)
+    return jsonify(resultD), 200
 #-----------------ENTRANCE-----------------------
 @app.route('/', methods=['GET'])
 def home():
     try: 
-        return render_template('index.html', title="page", jsonfile=json.dumps(resultD)) #{"test": 123}
+        return render_template('index.html', title="page", jsonfile=json.dumps(perform())) #{"test": 123}
     except Exception as e:
         pass
         # abort_msg(e)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)
-

app/calculator.py
@@ -5,67 +5,69 @@ import statistics as stat
 
 data_transforms = None
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = [DB_TYPE]+[DB_CONNECTOR]://[USERNAME]:[PASSWORD]@[HOST]:[PORT]/[DB_NAME]
-
-
-def load_data(cls_file, log_file):
-    global classes, megadata
-    # df = pd.read_json(r'file')
-    # djson = df.to_csv()
-    with open(cls_file, "r") as f:
-        djson = json.load(json_file)
-        # classes = f.read().split("\n")
-    with open(log_file, "r") as f:
-        megadata = f.read().split("\n")
-
-def load_manuplate():
-    global data_transform #JSON to NUMPY
-    # df = pd.read_csv(workbook_name.csv, sep=',',header=0)
-    # nmp = df.to_numpy()
-    trendObj = {
-    'all_vals': nmp[:,11],
-    'format_1': np.zeros(len(nmp[:,11])),
-    'format_2': np.zeros(len(nmp[:,11])),
-    'format_3': np.zeros(len(nmp[:,11])),
-    'format_4': np.zeros(len(nmp[:,11]))
-    }
-    return nmp, trendObj
-
-def load_model(path):
-    pass
-    # global model
-    # model = torch.load(path).to(device)
-
+# def load_data(cls_file, log_file):
+#     global classes, megadata
+#     # df = pd.read_json(r'file')
+#     # djson = df.to_csv()
+#     with open(cls_file, "r") as f:
+#         djson = json.load(json_file)
+#         # classes = f.read().split("\n")
+#     with open(log_file, "r") as f:
+#         megadata = f.read().split("\n")
+# def load_manuplate():
+#     global data_transform #JSON to NUMPY
+#     # df = pd.read_csv(workbook_name.csv, sep=',',header=0)
+#     # nmp = df.to_numpy()
+#     trendObj = {
+#     'all_vals': nmp[:,11],
+#     'format_1': np.zeros(len(nmp[:,11])),
+#     'format_2': np.zeros(len(nmp[:,11])),
+#     'format_3': np.zeros(len(nmp[:,11])),
+#     'format_4': np.zeros(len(nmp[:,11]))
+#     }
+#     return nmp, trendObj
+# def load_model(path):
+#     pass
+# # global model
+# # model = torch.load(path).to(device)
 
 # @app.route("/predict", methods=["POST"])
 # def metrics():
-
 #     output_dict = {"success": False}
 #     if flask.request.method == "POST":
 #         data = flask.request.json
-        
-#         # read the image in PIL format
-#         response = requests.get(data["image"])
-#         image = Image.open(BytesIO(response.content))
-
-#         # transform image
-#         image_tensor = data_transforms(image).float()
-#         image_tensor = image_tensor.unsqueeze_(0).to(device)
-
-#         # predict and max
-#         output = model(image_tensor)
-#         _, predicted = torch.max(output.data, 1)
-#         output_dict["predictions"] = classes[predicted]
+#     output_dict = {"success": False}
+#     if flask.request.method == "GET":
+#         output_dict["calc"] = calc
 #         output_dict["success"] = True
 #     return flask.jsonify(output_dict), 200
-keys = ["Cp","Cpu","Cpk","Ppk"]
+        
+#         # read the image in PIL format
+#     response = requests.get(data["image"])
+#     image = Image.open(BytesIO(response.content))
+
+#     # transform image
+#     image_tensor = data_transforms(image).float()
+#     image_tensor = image_tensor.unsqueeze_(0).to(device)
+
+#     # predict and max
+#     output = model(image_tensor)
+#     _, predicted = torch.max(output.data, 1)
+#     output_dict["predictions"] = classes[predicted]
+#     output_dict["success"] = True
+#     return flask.jsonify(output_dict), 200
+
+
 def calc(mylst,usl,lsl):
     # Moving average:
+    # global results
+    ANS = 0,0,0,0
     try:
-
         arr = np.array(mylst)
+        print("arr: ",arr)
         arr = arr.ravel()
-        ngroup = 5 #input() #給使用者指定每組大小
+        print("ravel, " ,arr)
+        ngroup = 10 #input() #給使用者指定每組大小
         ppkarr = np.array_split(arr,ngroup)# 將資料分組計算
         ppkarrSig = [np.mean(i) for i in ppkarr]
         # for i in ppkarr:
@@ -80,17 +82,12 @@ def calc(mylst,usl,lsl):
         ppu = float(usl - m) / (3*sigmaPpk)
         ppl = float(m - lsl) / (3*sigmaPpk)
         Ppk = np.min([ppu,ppl])
-    # print(Cp, Cpu, Cpk, Ppk)
-        results = Cp, Cpu, Cpk, Ppk
-    except:
+        ANS = Cp, Cpu, Cpk, Ppk
+    # print(Cp, Cpu, Cpk, Ppk) # d = dict([(x,ANS[x]) for x in range(len(ANS))])
+    except  Exception as e: # work on python 2.x
+        print('Failed to upload to ftp: '+ str(e))
         print("fix infinity")
-    return results # d = dict([(x,results[x]) for x in range(len(results))])
-
-    # output_dict = {"success": False}
-    # if flask.request.method == "GET":
-    #     output_dict["calc"] = calc
-    #     output_dict["success"] = True
-    # return flask.jsonify(output_dict), 200
+    return ANS #Cp, Cpu, Cpk, Ppk
 
 def testRule1(obj,newNum, mean, sd):
 